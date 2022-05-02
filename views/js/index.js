2f7e7c63a314c423f802a30e4ea1f92ad3717d45
medwin49-ai <mendoza_edwin@student.smc.edu>
2022-05-02 13:33:32 -0700

    I've added functionality to the text chat

--- views/js/index.js
+++ views/js/index.js
@@ -1,6 +1,32 @@
-document.getElementById("mp").addEventListener("click", )
+const socket = io()
+
+document.getElementById("user-button").addEventListener("click" , function(){
+    let username = document.getElementById("user-id").value
+    var hideInput = document.querySelector(".user-input")
+    var singleplay = document.getElementById("spb")
+    var multiplPlayer = document.getElementById("mp")
+    console.log(username)
+    if (hideInput.style.display === "none") {
+       hideInput.style.display ="block"
+        
+      } else {
+        hideInput.style.display = "none";
+        singleplay.style.display = "block";
+        multiplPlayer.style.display = "block"
+      }
+    
+    let p = document.createElement("p")
+    let node = document.createTextNode("Welcome, " + username)
+    p.appendChild(node)
+    document.body.appendChild(p)
+    socket.emit("user-information" , username)
+})
 
 
-function test(){
-    console.log("it worked")
-}
+
+
+
+socket.on("connect", () =>{
+    console.log(socket.id)
+})
+
